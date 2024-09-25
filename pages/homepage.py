5a1a2900a3ff7cfe0add9936fb566c0365ca141b
Risshab Srinivas <risshabsrinivas@Risshabs-Air.lan>
2024-09-25 14:29:56 +0530

    added

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,5 +1,5 @@
 import streamlit as st
 print("Hello")
-st.write("My first page")
 st.write("This is Risshab's personal page.")
+st.write("Hello world!.")
 number = st.slider("Pick a number", 0, 100)
